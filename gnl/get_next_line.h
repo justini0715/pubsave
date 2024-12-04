3245a0335f44dab631c8654d9f1f9da4237c9b06
hubril <hubril@test.com>
2024-12-04 17:39:01 +0900

    latest

---
+++ gnl/get_next_line.h
@@ -0,0 +1,26 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
